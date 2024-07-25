65f54a6a9a06c002de057326c08dd901aaacb76d
nadavgl <ngliksht@gmail.com>
2024-07-25 12:47:39 -0400

    removed node modules

---
+++ index.js
@@ -0,0 +1,35 @@
+const { Client } = require('pg')
+const inquirer = require('inquirer')
+const client = new Client({
+    user: 'postgres',
+    password: 'pass',
+    database: 'students_db'
+})
+
+client.connect()
+    .then(async () => {
+        console.log('db connected')
+        const answerObj = await inquirer.prompt([
+            {
+                name: 'first_name',
+                message: 'Enter the student\'s first name',
+            },
+            {
+                name: 'last_name',
+                message: 'Enter the student\'s last name',
+            },
+            {
+                name: 'course_name',
+                message: 'Enter the student\'s course name',
+            }
+        ])
+
+        const sql = `INSERT INTO students (first_name, last_name, course_name) VALUES($1, $2, $3)`;
+
+        await client.query(sql, [answerObj.first_name, answerObj.last_name, answerObj.course_name])
+
+        console.log('Student Created')
+
+
+
+    })
