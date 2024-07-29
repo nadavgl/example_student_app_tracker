6bdc34a16ff853073f8bc09873268c2e14eaedf5
nadavgl <ngliksht@gmail.com>
2024-07-29 10:32:30 -0400

    added scripts

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,21 @@
 const { Pool } = require('pg')
+const is_prod = process.env.NODE_ENV === 'production'
 
-
-const client = new Pool({
+const localConnection={
     user: 'postgres',
     password: 'pass',
     database: 'student_app_db'
-})
+}
+
+const renderConnection = {
+    connectionString: process.env.DB_URL,
+    ssl:{
+        rejectUnauthorized: false
+    },
+}
+
+const connectObj = is_prod ? renderConnection : localConnection;
+
+const client = new Pool (connectObj)
 
 module.exports = client
